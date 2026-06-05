9994f9ebecc6bb9484227f618995a148e8720b5e
majdkhazen <119238662+majdkhazen@users.noreply.github.com>
2026-06-05 11:04:53 +0300

    Add: count-paths - submission-0

---
+++ Data Structures & Algorithms/count-paths/submission-0.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+      vector<vector<int>> grid(m,vector<int>(n));   
+    
+    for(int i=0; i<n; i++){
+        grid[0][i] = 1;
+    }
+    for(int i=0; i<m; i++){
+        grid[i][0] = 1;
+    }
+
+    for(int i=1;i<n;i++){
+        for(int j=1; j<m; j++){
+            grid[j][i] = grid[j-1][i]+grid[j][i-1];
+        }
+    }
+    return grid[m-1][n-1];
+    }
+};
